685572859d12d40feacbeed2a2fd291e4c4472e7
Pavel Nikolaichev <pavelnikolaichev@gmail.com>
2025-07-11 07:54:58 +0800

    Fixed the issues

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use dotenv::dotenv;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 use log::{info, error};
@@ -7,25 +8,32 @@ async fn handle_connection(mut stream: TcpStream) {
     info!("accepted new connection: {:?}", stream.peer_addr());
 
     let mut buffer = [0; 1024];
-    match stream.read(&mut buffer).await {
-        Ok(0) => {
-            info!("connection closed: {:?}", stream.peer_addr());
-        }
-        Ok(n) => {
-            info!("received {} bytes from {:?}", n, stream.peer_addr());
-
-            if let Err(e) = stream.write_all(b"+PONG\r\n").await {
-                error!("error writing to stream: {}", e);
+    loop {
+        match stream.read(&mut buffer).await {
+            Ok(0) => {
+                info!("connection closed by client: {:?}", stream.peer_addr());
+                return;
             }
-        }
-        Err(e) => {
-            error!("error reading from stream: {}", e);
+            Ok(n) => {
+                info!("received {} bytes from {:?}", n, stream.peer_addr());
+
+                if let Err(e) = stream.write_all(b"+PONG\r\n").await {
+                    error!("error writing to stream: {}", e);
+                    return;
+                }
+            }
+            Err(e) => {
+                error!("error reading from stream: {}", e);
+                return;
+            }
         }
     }
 }
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+    env_logger::init();
     info!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
